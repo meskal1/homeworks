6e1a9063122b262819d8e76dbd3751439ed5e231
Daniil <meskal1@rambler.ru>
2022-03-24 22:19:59 +0300

    homework4

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HW1 from '../../../p2-homeworks/h1/HW1'
 import HW2 from '../../../p2-homeworks/h2/HW2'
 import HW3 from '../../../p2-homeworks/h3/HW3'
+import HW4 from '../../../p2-homeworks/h4/HW4'
 import s from './App.module.css'
 
 export function App() {
@@ -11,7 +12,7 @@ export function App() {
 			<HW1 />
 			<HW2 />
 			<HW3 />
-			{/*<HW4/>*/}
+			<HW4 />
 			{/*<HW5/>*/}
 
 		</div>
