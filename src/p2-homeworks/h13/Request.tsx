3fc89389b16deac9f79290ca17a33073befb8551
Daniel <meskal1@rambler.ru>
2022-11-15 15:27:27 +0300

    HW13_added_pending_check

--- src/p2-homeworks/h13/Request.tsx
+++ src/p2-homeworks/h13/Request.tsx
@@ -1,4 +1,3 @@
-import { error } from 'console'
 import React, { useState } from 'react'
 import s from './Request.module.scss'
 import { RequestsAPI } from './RequestsAPI'
@@ -8,15 +7,19 @@ type RequestType = {}
 export const Request: React.FC<RequestType> = () => {
   const [checkboxState, setCheckboxState] = useState(false)
   const [sendStatus, setSendStatus] = useState('')
+  const [isPending, setIsPending] = useState(false)
 
   const onClickButtonHandler = () => {
+    setIsPending(true)
     RequestsAPI.postCheckboxValue(checkboxState)
       .then(res => {
-        setSendStatus('Успешный запрос')
+        setIsPending(false)
+        setSendStatus(res.data.errorText + ' ' + res.data.info)
         console.log(res.data)
       })
       .catch(error => {
-        setSendStatus('Ошибка запроса')
+        setIsPending(false)
+        setSendStatus(error.response.data.errorText)
         console.log({ ...error })
         console.log(error.response ? error.response.data.errorText : error.message)
       })
@@ -32,7 +35,7 @@ export const Request: React.FC<RequestType> = () => {
       <div className={s.content}>
         <input type='checkbox' checked={checkboxState} onChange={onChangeInputHandler} />
         <span>Статус отправки: {sendStatus}</span>
-        <button className={s.button} onClick={onClickButtonHandler}>
+        <button className={s.button} onClick={onClickButtonHandler} disabled={isPending}>
           Отправить запрос
         </button>
       </div>
--- src/p2-homeworks/h13/RequestsAPI.ts
+++ src/p2-homeworks/h13/RequestsAPI.ts
@@ -4,8 +4,15 @@ const instance = axios.create({
   baseURL: 'https://neko-cafe-back.herokuapp.com/auth/test',
 })
 
+type ResponseType = {
+  errorText: string
+  info: string
+  yourBody: { success: string }
+  yourQuery: {}
+}
+
 export const RequestsAPI = {
   postCheckboxValue(checkboxValue: boolean) {
-    return instance.post('', { success: checkboxValue })
+    return instance.post<ResponseType>('', { success: checkboxValue })
   },
 }
